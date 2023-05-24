110ba1f406ecb493888e642bf5a0c65ce7580483
Hamed <hamedmasoudi95@gmail.com>
2023-05-24 19:50:36 +0430

    Calculating the cumulative reward for each episode

--- SARSA.py
+++ SARSA.py
@@ -1,5 +1,7 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 env = gym.make('Taxi-v3') # I used version 0.21.0.
 
@@ -37,3 +39,28 @@ for i in range(num_episodes):
         q_table[state][action] += alpha * (reward + gamma * q_table[next_state][next_action] - q_table[state][action])
         
         state = next_state
+
+# Calculating the cumulative reward for each episode
+rewards = []
+episode = []
+cumulative_reward = 0
+state = env.reset()
+done = False
+
+for ep in range (100):
+    cumulative_reward = 0
+    state = env.reset()
+    done = False
+    while not done:
+        action = np.argmax(q_table[state])
+        next_state, reward, done, info = env.step(action)
+        state = next_state
+        cumulative_reward += reward
+    rewards.append(cumulative_reward)
+    episode.append(ep)
+
+plt.plot(episode, rewards)
+plt.xlabel('Episode')
+plt.ylabel('Cumulative Reward')
+plt.title('Q-Learning Performance')
+plt.show()
